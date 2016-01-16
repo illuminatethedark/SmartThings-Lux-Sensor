8f22bff0bb351920eaba1c0823aec23a0f79a2d9
illuminatethedark <danielmarcus@DanielsMac2014.local>
2016-01-16 11:36:16 -0500

    Initial Commit
    Illuminance sensor for Smartthings Arduino shield.

---
+++ ST_lx_sensor.ino
@@ -0,0 +1,131 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+#include <SoftwareSerial.h>
+#include <SmartThings.h>
+   
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+
+void displaySensorDetails() {
+  sensor_t sensor;
+  tsl.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" lux");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" lux");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" lux");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+void configureSensor() {
+  /* You can also manually set the gain or enable auto-gain support */
+  // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
+  // tsl.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
+  tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
+  
+  /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
+
+  /* Update these values depending on what you've set above! */  
+  Serial.println("------------------------------------");
+  Serial.print  ("Gain:         "); Serial.println("Auto");
+  Serial.print  ("Timing:       "); Serial.println("13 ms");
+  Serial.println("------------------------------------");
+}
+
+SmartThingsCallout_t messageCallout;    // call out function forward decalaration
+SmartThings smartthing;
+
+bool isDebugEnabled;    // enable or disable debug in this example
+int stateNetwork;       // state of the network 
+
+void setNetworkStateLED()
+{
+  SmartThingsNetworkState_t tempState = smartthing.shieldGetLastNetworkState();
+  if (tempState != stateNetwork)
+  {
+    switch (tempState)
+    {
+      case STATE_NO_NETWORK:
+        if (isDebugEnabled) Serial.println("NO_NETWORK");
+        smartthing.shieldSetLED(2, 0, 0); // red
+        break;
+      case STATE_JOINING:
+        if (isDebugEnabled) Serial.println("JOINING");
+        smartthing.shieldSetLED(2, 0, 0); // red
+        break;
+      case STATE_JOINED:
+        if (isDebugEnabled) Serial.println("JOINED");
+        smartthing.shieldSetLED(0, 0, 0); // off
+        break;
+      case STATE_JOINED_NOPARENT:
+        if (isDebugEnabled) Serial.println("JOINED_NOPARENT");
+        smartthing.shieldSetLED(2, 0, 2); // purple
+        break;
+      case STATE_LEAVING:
+        if (isDebugEnabled) Serial.println("LEAVING");
+        smartthing.shieldSetLED(2, 0, 0); // red
+        break;
+      default:
+      case STATE_UNKNOWN:
+        if (isDebugEnabled) Serial.println("UNKNOWN");
+        smartthing.shieldSetLED(0, 2, 0); // green
+        break;
+    }
+    stateNetwork = tempState; 
+  }
+}
+
+void setup(void) 
+{
+  isDebugEnabled = true;
+  stateNetwork = STATE_JOINED;  // set to joined to keep state off if off
+  Serial.begin(9600);
+  Serial.println("Light Sensor Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!tsl.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  
+  /* Setup the sensor gain and integration time */
+  configureSensor();
+  
+  /* We're ready to go! */
+  Serial.println("");
+}
+
+void loop(void) 
+{
+  smartthing.run;
+  setNetworkStateLED();
+    
+  /* Get a new sensor event */ 
+  sensors_event_t event;
+  tsl.getEvent(&event);
+ 
+  /* Display the results (light is measured in lux) */
+  if (event.light)
+  {
+    Serial.print(event.light); Serial.println(" lux");
+  }
+  else
+  {
+    /* If event.light = 0 lux the sensor is probably saturated
+       and no reliable data could be generated! */
+    Serial.println("Sensor overload");
+  }
+  delay(1000);
+}
